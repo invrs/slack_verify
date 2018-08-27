830faa4f552285c28ee80c2d9825be2af8d6c26a
claytongentry <claytongentry2017@u.northwestern.edu>
2018-08-27 18:05:19 -0400

    relax plug requirement

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule SlackVerify.MixProject do
       app: :slack_verify,
       description: description(),
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -25,7 +25,7 @@ defmodule SlackVerify.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:plug, "~> 1.6"},
+      {:plug, "~> 1.0"},
 
       {:ex_doc, "~> 0.18.0", only: :dev}
     ]
