9e22403a52b5118e043d3f7d0260451d778f4231
claytongentry <claytongentry2017@u.northwestern.edu>
2018-08-27 17:33:56 -0400

    implement verification protocol

--- lib/slack_verify.ex
+++ lib/slack_verify.ex
@@ -1,18 +1,47 @@
 defmodule SlackVerify do
+
+  import Plug.Conn
+
   @moduledoc """
-  Documentation for SlackVerify.
+  SlackVerify is a plug for verifying the
+  authenticity of requests from Slack.
+
+  It follows the protocol described in Slack's verification docs:
+  https://api.slack.com/docs/verifying-requests-from-slack
   """
 
-  @doc """
-  Hello world.
+  @version "v0"
 
-  ## Examples
+  def init(opts), do: opts
 
-      iex> SlackVerify.hello
-      :world
+  def call(conn, opts) do
+    slack_signing_secret = Keyword.fetch!(opts, :slack_signing_secret)
 
-  """
-  def hello do
-    :world
+    case Plug.Conn.read_body(conn) do
+      {:ok, body, conn} ->
+        { signature, timestamp } = get_headers(conn)
+        sig_basestring = Enum.join([@version, timestamp, body], ":")
+
+        hmac = sha256(slack_signing_secret, sig_basestring)
+        if valid_signature?(hmac, signature),
+          do: conn, else: halt(conn)
+    end
+  end
+
+  defp valid_signature?(hmac, [ signature ]),
+    do: String.downcase("#{@version}=#{hmac}") == signature
+  defp valid_signature?(_hmac, _signature), do: false
+
+  defp sha256(key, string) do
+    :sha256
+    |> :crypto.hmac(key, string)
+    |> Base.encode16
+  end
+
+  defp get_headers(conn) do
+    {
+      Plug.Conn.get_req_header(conn, "x-slack-signature"),
+      Plug.Conn.get_req_header(conn, "x-slack-request-timestamp")
+    }
   end
 end
--- test/slack_verify_test.exs
+++ test/slack_verify_test.exs
@@ -1,8 +1,24 @@
 defmodule SlackVerifyTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  import Plug.Conn, only: [ put_req_header: 3 ]
+  use Plug.Test
+
   doctest SlackVerify
 
-  test "greets the world" do
-    assert SlackVerify.hello() == :world
+  # dummy key and signature provided in verification walkthrough
+  @secret "8f742231b10e8888abcd99yyyzzz85a5"
+  @signature "v0=a2114d57b48eac39b9ad189dd8316235a7b4a8d21a10bd27519666489c69b503"
+
+  test "verifies a Slack request" do
+    body = File.read!("./test/fixtures/body.txt") |> String.trim()
+    conn =
+      conn(:post, "/", body)
+      |> put_req_header("x-slack-request-timestamp", "1531420618")
+      |> put_req_header("x-slack-signature", @signature)
+
+    opts = SlackVerify.init([slack_signing_secret: @secret])
+    conn = SlackVerify.call(conn, opts)
+
+    refute conn.halted
   end
 end
